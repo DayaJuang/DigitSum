a03a48bb441e931b1e954110031e948b5cbdf999
daya <dayajuangmahaputra@gmail.com>
2022-11-09 16:04:49 +0900

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,25 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int ans = sumDigits(244);
+
+        System.out.println(ans);
     }
+
+    public static int sumDigits(int num){
+        // Check if the number is greater or equal to 10
+        if(num < 10)
+            return -1;
+
+        // Calculate the sum
+        int sum = 0;
+        while(num > 0){
+            sum += (num % 10);
+            num /= 10;
+        }
+
+        return sum;
+    }
+
 }
